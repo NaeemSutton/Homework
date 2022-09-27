90e690a35c6cd2e6563db1f15bd8080ef03307ea
NaeemSutton <107595797+NaeemSutton@users.noreply.github.com>
2022-09-27 12:14:58 -0700

    Update change.cpp

--- change.cpp
+++ change.cpp
@@ -18,37 +18,67 @@ int main() {
     cin >> total;
 
     if(total == 0) {
-        cout << "no change" << endl;
+        cout << "No change" << endl;
     }
 
-   else if (total >= 100) {
+   if (total >= 100 && total > 200) {
         totalDollars = total / dollars;
         total = total - (dollars * totalDollars);
-        cout << totalDollars << " dollars" << endl;
+        cout << totalDollars << " Dollars" << endl;
+    }
+    
+    else if (total >= 100) {
+        totalDollars = total / dollars;
+        total = total - (dollars * totalDollars);
+        cout << totalDollars << " Dollar" << endl;
     }
 
-    else if (total >= 25 && total < 100) {
+    if (total >= 25 && total < 100 && total >= 50) {
         totalQuarters = total / quarters;
         total = total - (quarters * totalQuarters);
-        cout << totalQuarters << " quarters" << endl;
+        cout << totalQuarters << " Quarters" << endl;
+    }
+    
+    else if (total >= 25 && total < 100 ) {
+        totalQuarters = total / quarters;
+        total = total - (quarters * totalQuarters);
+        cout << totalQuarters << " Quarter" << endl;
     }
 
+    if (total >= 10 && total < 25 && total >= 20) {
+        totalDimes = total / dimes;
+        total = total - (dimes * totalDimes);
+        cout << totalDimes << " Dimes" << endl;
+    }
+    
     else if (total >= 10 && total < 25) {
         totalDimes = total / dimes;
         total = total - (dimes * totalDimes);
-        cout << totalDimes << " dimes" << endl;
+        cout << totalDimes << " Dime" << endl;
     }
 
+    if (total >= 5 && total < 10 && nickels >= 10) {
+        totalNickels = total / nickels;
+        total = total - (nickels * totalNickels);
+        cout << totalNickels << " Nickels" << endl;
+    }
+    
     else if (total >= 5 && total < 10) {
         totalNickels = total / nickels;
         total = total - (nickels * totalNickels);
-        cout << totalNickels << " nickels" << endl;
+        cout << totalNickels << " Nickel" << endl;
     }
 
-    else if (total >= 1 && total < 5) {
+    if (total >= 2 && total < 5  ) {
         totalPennies = total / pennies;
         total = total - (pennies * totalPennies);
-        cout << totalPennies << " pennies" << endl;
+        cout << totalPennies << " Pennies" << endl;
+    }
+    
+    else if (total > 0 && total < 2  ) {
+        totalPennies = total / pennies;
+        total = total - (pennies * totalPennies);
+        cout << totalPennies << " Penny" << endl;
     }
 
     return 0;
